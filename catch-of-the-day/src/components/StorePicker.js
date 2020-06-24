96d87540ec7fa43b27327124dc8587b23fe15f4f
Joseph Castle <joseph.castle@gsa.gov>
2020-06-24 11:48:24 -0400

    Updates to AddFishForm and npm modules

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -6,7 +6,7 @@ class StorePicker extends React.Component {
 
     goToStore = (event) => {
         event.preventDefault();
-        const storeName = this.myInput.value.value;
+        const storeName = this.myInput.current.value;
         this.props.history.push(`/store/${storeName}`); 
     }
     render() {
