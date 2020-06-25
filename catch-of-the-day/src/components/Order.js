9c7e5b99226d5fbef0c1f5f70e293814392fc136
Joseph Castle <joseph.castle@gsa.gov>
2020-06-25 12:05:05 -0400

    Latest updates to Catch of the Day app

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
+    renderOrder = (key) => {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        const isAvailable = fish.status === 'available';
+        if (!isAvailable) {
+           return <li key={key}>Sorry { fish ? fish.name : fish } is no longer available.</li>
+        }
+        return ( <li key={key}>
+            {count} lbs {fish.name}
+            {formatPrice(count * fish.price) }
+        </li>
+        );
+    }
     render() {
+        const orderIDs = Object.keys(this.props.order);
+        const total = orderIDs.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status === 'available';
+            if (isAvailable) {
+                return prevTotal + (count * fish.price);
+            }
+            return prevTotal;
+        }, 0); 
         return (
-            <div className="order">Order!!!</div>
+            <div className="order-wrap ">
+                <h2>Order</h2>
+                <ul className="order">{orderIDs.map(this.renderOrder)}</ul>
+                <div className="total">Total:
+                <strong>{formatPrice(total)}</strong></div>
+            </div>
         );
     }
 }
