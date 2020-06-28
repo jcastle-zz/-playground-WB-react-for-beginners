3ba36a00eeba9a95b95b3ca2e3563684d9bf80d8
Joseph Castle <joseph.castle@gsa.gov>
2020-06-28 14:59:32 -0400

    Update to Edit Fish Form

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 class EditFishForm extends React.Component {
+    handleChange = (event) => {
+         console.log(event);
+    }
     render() {
         return ( 
             <div className="fish-edit">
-                <input type="text" name="name" />
-                <input type="text" name="price" />
-                <input type="text" name="status" />
-                <input type="text" name="desc" />
-                <input type="text" name="image" />
+                <input type="text" name="name"  onChange={this.handleChange} value={this.props.fish.name} />
+                <input type="text" name="price"  onChange={this.handleChange} value={this.props.fish.price} />
+                <select type="text" name="status" onChange={this.handleChange} value={this.props.fish.status} >
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc" onChange={this.handleChange} value={this.props.fish.desc} />
+                <input type="text" name="image"  onChange={this.handleChange} value={this.props.fish.image} />
             </div>
         )
     }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -7,7 +7,7 @@ class Inventory extends React.Component {
         return (
             <div className="inventory">
             <h2>Inventory</h2>
-            {Object.keys(this.props.fishes).map(fish => <EditFishForm / >)}
+            {Object.keys(this.props.fishes).map(key => <EditFishForm key={key} fish={this.props.fishes[key]}/>)}
             <AddFishForm addFish={this.props.addFish} />
             <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button> 
             </div>
